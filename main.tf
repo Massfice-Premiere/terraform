c10e8c673ca49ae91d86c73028aab450594120aa
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-11 22:53:52 +0100

    adding repo

--- main.tf
+++ main.tf
@@ -16,21 +16,21 @@ provider "github" {
   token = var.GITHUB_TOKEN
 }
 
-# resource "github_repository" "repository" {
-#   name        = "example-terraform-repository"
-#   description = "example-terraform-repository"
-#   visibility  = "private"
-#   auto_init   = true
-# }
+resource "github_repository" "repository" {
+  name        = "example-terraform-repository"
+  description = "example-terraform-repository"
+  visibility  = "private"
+  auto_init   = true
+}
 
-# resource "github_repository_file" "create-sealed-secret" {
-#   repository     = github_repository.repository.name
-#   branch         = "main"
-#   file           = ".github/workflows/create-sealed-secret.yaml"
-#   content        = file(".github/workflows/create-sealed-secret.yaml")
-#   commit_message = "Added sealed secret workflow"
+resource "github_repository_file" "create-sealed-secret" {
+  repository     = github_repository.repository.name
+  branch         = "main"
+  file           = ".github/workflows/create-sealed-secret.yaml"
+  content        = file(".github/workflows/create-sealed-secret.yaml")
+  commit_message = "Added sealed secret workflow"
 
-#   provisioner "local-exec" {
-#     command = "curl --location --request POST 'https://api.github.com/repos/massficePremiere/${github_repository.repository.name}/dispatches' --header 'Accept: application/vnd.github.everest-preview+json' --header 'Authorization: Bearer ${var.GITHUB_TOKEN}' --header 'Content-Type: application/json' --data-raw '{\"event_type\": \"create-sealed-secret\"}'"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "curl --location --request POST 'https://api.github.com/repos/massficePremiere/${github_repository.repository.name}/dispatches' --header 'Accept: application/vnd.github.everest-preview+json' --header 'Authorization: Bearer ${var.GITHUB_TOKEN}' --header 'Content-Type: application/json' --data-raw '{\"event_type\": \"create-sealed-secret\"}'"
+  }
+}
