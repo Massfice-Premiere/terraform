0e2aa05ab29b5e3c498b170e2a2ff485dad1d36d
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-11 19:44:11 +0100

    added sealed secrets to created repo

--- main.tf
+++ main.tf
@@ -11,9 +11,20 @@ provider "github" {
   owner = "massficePremiere"
 }
 
-# resource "github_repository" "repository" {
-#   name        = "example-terraform-repository"
-#   description = "example-terraform-repository"
-#   visibility  = "private"
+resource "github_repository" "repository" {
+  name        = "example-terraform-repository"
+  description = "example-terraform-repository"
+  visibility  = "private"
+}
 
-# }
+resource "github_repository_file" "create-sealed-secret" {
+  depends_on = [
+    github_repository.repository
+  ]
+
+  repository     = github_repository.repository.name
+  branch         = "main"
+  file           = "./.githubworkflows/create-sealed-secret.yaml"
+  content        = file("./.githubworkflows/create-sealed-secret.yaml")
+  commit_message = "Added sealed secret workflow"
+}
