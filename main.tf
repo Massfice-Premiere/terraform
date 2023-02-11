4b29a3d320b1e65b6237106f5a66ae323b17781a
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-11 18:05:33 +0100

    added repository

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {}
+
+resource "github_repository" "repository" {
+  name        = "example-terraform-repository"
+  description = "example-terraform-repository"
+  visibility  = "private"
+}
