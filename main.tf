2a716f4814475e65c76ca00f10d1b023a2cf48bc
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-22 15:01:21 +0100

    fixing adding ingress

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ module "kubernetes_module" {
   kubernetes_host        = module.digitalocean_module.kubernetes_host
   kubernetes_token       = module.digitalocean_module.kubernetes_token
   kubernetes_certificate = module.digitalocean_module.kubernetes_certificate
-  loaloadbalancer_id     = module.digitalocean_module.loadbalancer_id
+  loadbalancer_id        = module.digitalocean_module.loadbalancer_id
 }
 
