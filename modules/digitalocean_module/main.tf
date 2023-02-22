99a7a85196e751f9f7daa768b7b406da06184c3b
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-22 14:20:04 +0100

    added loadbalancer and removed github

--- modules/digitalocean_module/main.tf
+++ modules/digitalocean_module/main.tf
@@ -21,3 +21,25 @@ resource "digitalocean_kubernetes_cluster" "kubernetes" {
     node_count = 3
   }
 }
+
+resource "digitalocean_loadbalancer" "loadbalancer" {
+  name      = "${var.cluster_name}-loadbalancer"
+  region    = "fra1"
+  size      = "lb-small"
+  algorithm = "round_robin"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+
+  }
+
+  lifecycle {
+    ignore_changes = [
+      forwarding_rule,
+    ]
+  }
+}
