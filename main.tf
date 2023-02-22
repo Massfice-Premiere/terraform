6a5e88b66e7241c826270850729ea28a991eaf42
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-22 14:45:22 +0100

    trying to fix

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {}
 }
 
-# module "digitalocean_module" {
-#   source = "./modules/digitalocean_module"
+module "digital_ocean_module" {
+  source = "./modules/digitalocean_module"
 
-#   token = var.digitalocean_token
-# }
+  token = var.digitalocean_token
+}
 
 
 # module "kubernetes_module" {
