667fec8ebf9539f991bbb41005ed29a78e27d0bd
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-22 14:40:34 +0100

    fixing

--- main.tf
+++ main.tf
@@ -2,18 +2,19 @@ terraform {
   required_providers {}
 }
 
-module "digitalocean_module" {
+module "digital_ocean_module" {
   source = "./modules/digitalocean_module"
 
   token = var.digitalocean_token
 }
 
+
 module "kubernetes_module" {
   source = "./modules/kubernetes_module"
 
-  kubernetes_host        = module.digitalocean_module.kubernetes_host
-  kubernetes_token       = module.digitalocean_module.kubernetes_token
-  kubernetes_certificate = module.digitalocean_module.kubernetes_certificate
-  loadbalancer_ip        = module.digitalocean_module.loadbalancer_ip
+  kubernetes_host        = module.digital_ocean_module.kubernetes_host
+  kubernetes_token       = module.digital_ocean_module.kubernetes_token
+  kubernetes_certificate = module.digital_ocean_module.kubernetes_certificate
+  loadbalancer_ip        = module.digital_ocean_module.loadbalancer_ip
 }
 
