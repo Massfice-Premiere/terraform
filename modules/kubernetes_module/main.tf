443e1d662963156abbc8a04ac2dce71c992804de
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-23 16:20:54 +0100

    changing version for cert manager

--- modules/kubernetes_module/main.tf
+++ modules/kubernetes_module/main.tf
@@ -80,6 +80,7 @@ resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
+  version    = "v1.11"
   namespace  = "letsencrypt"
   timeout    = 120
 
