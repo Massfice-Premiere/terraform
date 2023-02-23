31f2157c5017f252b6c778ae7bdd51784dbac497
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-23 16:19:09 +0100

    deleting version from cert manager

--- modules/kubernetes_module/main.tf
+++ modules/kubernetes_module/main.tf
@@ -80,7 +80,6 @@ resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.0.1"
   namespace  = "letsencrypt"
   timeout    = 120
 
