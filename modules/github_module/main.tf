166e927af15813967adff0936a8c0794c86987eb
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-02-26 08:26:31 +0100

    moving files to argocd repo

--- modules/github_module/main.tf
+++ modules/github_module/main.tf
@@ -30,3 +30,27 @@ resource "github_repository_webhook" "argocd-webhook" {
     content_type = "json"
   }
 }
+
+resource "github_repository_file" "chart-yaml" {
+  repository     = var.argocd-repo
+  branch         = "main"
+  file           = "apps/argocd/Chart.yaml"
+  content        = file("../../charts/argocd-application/Chart.yaml")
+  commit_message = "Argo CD Application | Chart.yaml | Managed by Terraform"
+}
+
+resource "github_repository_file" "application-yaml" {
+  repository     = var.argocd-repo
+  branch         = "main"
+  file           = "apps/argocd/templates/application.yaml"
+  content        = file("../../charts/argocd-application/templates/application.yaml")
+  commit_message = "Argo CD Application | application.yaml | Managed by Terraform"
+}
+
+resource "github_repository_file" "values-yaml" {
+  repository     = var.argocd-repo
+  branch         = "main"
+  file           = "apps/argocd/templates/values.yaml"
+  content        = "repo_url: git@github.com:${var.owner}/${var.argocd-repo}.git\n"
+  commit_message = "Argo CD Application | values.yaml | Managed by Terraform"
+}
