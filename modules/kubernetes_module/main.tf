60fe18911fb70723c2f5143f41fbed52c5c7bc09
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-03-01 11:53:42 +0100

    cleaning

--- modules/kubernetes_module/main.tf
+++ modules/kubernetes_module/main.tf
@@ -192,11 +192,11 @@ resource "helm_release" "argocd-base" {
   }
 }
 
-resource "kubectl_manifest" "init-yaml" {
-  for_each  = toset(split("\n---\n", var.init-yaml))
-  yaml_body = each.key
+# resource "kubectl_manifest" "init-yaml" {
+#   for_each  = toset(split("\n---\n", var.init-yaml))
+#   yaml_body = each.key
 
-  depends_on = [
-    helm_release.argocd-base
-  ]
-}
+#   depends_on = [
+#     helm_release.argocd-base
+#   ]
+# }
