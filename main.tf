5919cc7ea5d5ac6a66cb02c41c709a05105e427c
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-03-01 01:11:27 +0100

    disabling temporary kubernetes

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {}
 }
 
-module "digitalocean_module" {
-  source = "./modules/digitalocean_module"
+# module "digitalocean_module" {
+#   source = "./modules/digitalocean_module"
 
-  token  = var.digitalocean_token
-  domain = var.domain
-}
+#   token  = var.digitalocean_token
+#   domain = var.domain
+# }
 
 module "github_module" {
   source = "./modules/github_module"
@@ -19,19 +19,19 @@ module "github_module" {
 }
 
 
-module "kubernetes_module" {
-  source = "./modules/kubernetes_module"
+# module "kubernetes_module" {
+#   source = "./modules/kubernetes_module"
 
-  kubernetes_host        = module.digitalocean_module.kubernetes_host
-  kubernetes_token       = module.digitalocean_module.kubernetes_config[0].token
-  kubernetes_certificate = module.digitalocean_module.kubernetes_config[0].cluster_ca_certificate
-  loadbalancer_id        = module.digitalocean_module.loadbalancer_id
-  loadbalancer_name      = module.digitalocean_module.loadbalancer_name
-  github_private_key     = module.github_module.private_key
-  github_repo_owner      = var.github_owner
-  github_argocd_repo     = var.github_argo_repo
-  letsencrypt_email      = var.letsencrypt_email
-  domain                 = var.domain
-  argocd_password        = var.argocd_password
-}
+#   kubernetes_host        = module.digitalocean_module.kubernetes_host
+#   kubernetes_token       = module.digitalocean_module.kubernetes_config[0].token
+#   kubernetes_certificate = module.digitalocean_module.kubernetes_config[0].cluster_ca_certificate
+#   loadbalancer_id        = module.digitalocean_module.loadbalancer_id
+#   loadbalancer_name      = module.digitalocean_module.loadbalancer_name
+#   github_private_key     = module.github_module.private_key
+#   github_repo_owner      = var.github_owner
+#   github_argocd_repo     = var.github_argo_repo
+#   letsencrypt_email      = var.letsencrypt_email
+#   domain                 = var.domain
+#   argocd_password        = var.argocd_password
+# }
 
