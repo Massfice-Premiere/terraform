48a2dd7cfdc68c2944031b095e90505803db3cdb
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-04-13 18:53:54 +0200

    secrets - part 22

--- modules/secret_module/outputs.tf
+++ modules/secret_module/outputs.tf
@@ -1,7 +1,7 @@
 output "secret" {
   value = {
     location = var.location,
-    yaml     = template_file.sealed-secret-yaml
+    yaml     = data.template_file.sealed-secret-yaml.rendered
     id       = "${var.namespace}/${var.name}.yaml"
   }
   description = "generated secret with path"
