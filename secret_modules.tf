d7a4045cbe0c7ccaa94b451c8f102a4545bd00e7
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-04-16 14:42:22 +0200

    trying to fix

--- secret_modules.tf
+++ secret_modules.tf
@@ -23,27 +23,21 @@ module "secret_module" {
 
   for_each = {
     database_prod_connection_for_example_namespace = {
-      name      = "database-connection"
+      name      = "database-connection-secret"
       namespace = "example"
       type      = "Opaque"
       location  = "apps/standard/example/database-connection-secret.yaml"
       data = {
-        MONGO_URI      = replace(module.mongodbatlas_module.prod-connection-string, "mongodb+srv://", "mongodb+srv://${module.mongodbatlas_module.prod-user-username}:${module.mongodbatlas_module.prod-user-password}@")
-        MONGO_HOST     = module.mongodbatlas_module.prod-connection-string
-        MONGO_USERNAME = module.mongodbatlas_module.prod-user-username
-        MONGO_PASSWORD = module.mongodbatlas_module.prod-user-password
+        MONGO_URI = replace(module.mongodbatlas_module.prod-connection-string, "mongodb+srv://", "mongodb+srv://${module.mongodbatlas_module.prod-user-username}:${module.mongodbatlas_module.prod-user-password}@")
       }
     }
     database_nonprod_connection_for_example2_namespace = {
-      name      = "database-connection"
+      name      = "database-connection-secret"
       namespace = "example2"
       type      = "Opaque"
       location  = "apps/standard/example2/database-connection-secret.yaml"
       data = {
-        MONGO_URI      = replace(module.mongodbatlas_module.nonprod-connection-string, "mongodb+srv://", "mongodb+srv://${module.mongodbatlas_module.nonprod-user-username}:${module.mongodbatlas_module.nonprod-user-password}@")
-        MONGO_HOST     = module.mongodbatlas_module.nonprod-connection-string
-        MONGO_USERNAME = module.mongodbatlas_module.nonprod-user-username
-        MONGO_PASSWORD = module.mongodbatlas_module.nonprod-user-password
+        MONGO_URI = replace(module.mongodbatlas_module.nonprod-connection-string, "mongodb+srv://", "mongodb+srv://${module.mongodbatlas_module.nonprod-user-username}:${module.mongodbatlas_module.nonprod-user-password}@")
       }
     }
     pull_secret_for_example_namespace = {
