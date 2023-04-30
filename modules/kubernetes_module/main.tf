6915f8568fa3a7777b0e5931859fc845e5a9faf8
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-04-30 16:25:40 +0200

    trying to fix

--- modules/kubernetes_module/main.tf
+++ modules/kubernetes_module/main.tf
@@ -254,17 +254,18 @@ resource "helm_release" "argocd-image-updater" {
     value = "true"
   }
 
-  set {
-    name  = "config.registries"
-    value = <<EOT
+  values = [
+    <<EOT
+config:
+  registries:
       - name: "Docker Hub"
         prefix: "docker.io"
         api_url: "https://registry-1.docker.io"
         credentials: "pullsecret:argocd/pull-secret"
         defaultns: "library"
         default: true
-    EOT
-  }
+EOT
+  ]
 }
 
 resource "helm_release" "sealed-secrets" {
